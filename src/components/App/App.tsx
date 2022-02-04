b7de0348a164cd5db75377c0772c6b5065d252cc
Edgar Mejia <edgar.mejia@rootstrap.com>
2022-02-04 11:02:28 -0500

    template and enviroment configuration

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import Main from 'components/Layout/Main';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Main.tsx
@@ -0,0 +1,5 @@
+function Main() {
+  return <div>MAIN WORKS!!</div>;
+}
+
+export default Main;
