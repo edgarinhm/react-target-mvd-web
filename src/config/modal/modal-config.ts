8ae899c61a7f40df8d34e907a1826ee343d04a5f
Edgar Mejia <edgarinhm@hotmail.com>
2022-04-16 21:14:35 -0500

    add  modal react library refactor theme select config file

---
+++ src/config/modal/modal-config.ts
@@ -0,0 +1,12 @@
+const modalConfig = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+export default modalConfig;
